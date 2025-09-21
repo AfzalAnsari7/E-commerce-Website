ab89bf0f1ac596134e7646a4600b54cef913b33c
AFZAL ANSARI <164075114+AfzalAnsari7@users.noreply.github.com>
2025-09-21 14:26:12 +0530

    Update product.js
    add demo products

--- frontend/src/data/product.js
+++ frontend/src/data/product.js
@@ -1,1 +1,26 @@
+// E-commerce-Website/frontend/src/data/product.js
+const demoProducts = [
+  {
+    id: 1,
+    title: "Demo Product 1",
+    price: 499,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "This is a sample product for demo purposes."
+  },
+  {
+    id: 2,
+    title: "Demo Product 2",
+    price: 799,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Another demo product with fake details."
+  },
+  {
+    id: 3,
+    title: "Demo Product 3",
+    price: 999,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+]
 
+export default demoProducts
