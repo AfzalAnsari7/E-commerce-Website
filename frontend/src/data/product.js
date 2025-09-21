502cd896eaa51f3ca3704fb9228f40223fef6674
AFZAL ANSARI <164075114+AfzalAnsari7@users.noreply.github.com>
2025-09-21 14:42:10 +0530

    Update product.js
    add more demo products

--- frontend/src/data/product.js
+++ frontend/src/data/product.js
@@ -21,6 +21,69 @@ const demoProducts = [
     image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
     description: "Use this only for frontend testing."
   }
+    {
+    id: 4,
+    title: "Demo Product 3",
+    price: 899,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 5,
+    title: "Demo Product 3",
+    price: 599,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 6,
+    title: "Demo Product 3",
+    price: 699,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 7,
+    title: "Demo Product 3",
+    price: 399,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 8,
+    title: "Demo Product 3",
+    price: 899,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 9,
+    title: "Demo Product 3",
+    price: 999,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 10,
+    title: "Demo Product 3",
+    price: 799,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 11,
+    title: "Demo Product 3",
+    price: 599,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
+  {
+    id: 12,
+    title: "Demo Product 3",
+    price: 699,
+    image: "https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco/9993da9f-2fb0-4a46-87c9-0dce89a12d63/W+NIKE+VOMERO+18.png",
+    description: "Use this only for frontend testing."
+  }
 ]
 
 export default demoProducts
